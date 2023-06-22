d7fbd332997f0d7dc0dd1d839960f7eed2da2893
Onixfff <danilnikonov01@mail.ru>
2023-06-22 14:26:24 +0200

    Семинар

---
+++ task01_squreNumber/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+
+a = 25; b = 5 -> да
+a = 2 b = 10 -> нет
+a = 9; b = -3 -> да
+a = -3 b = 9 -> нет
+*/
+
+Console.Write("Первое число - ");
+int numberFirst = Convert.ToInt32(Console.ReadLine());
+Console.Write("Второе число - ");
+int numberSecond = Convert.ToInt32(Console.ReadLine());
+
+if(numberFirst == numberSecond * numberSecond)
+    Console.WriteLine("true");
+else
+    Console.WriteLine("false");
