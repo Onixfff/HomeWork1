c017dd4d0c3ca1f8f6fb976a5b1ae9c574b4187e
Onixfff <danilnikonov01@mail.ru>
2023-06-26 14:48:58 +0200

    завершил 15 задачу

---
+++ task15/Program.cs
@@ -0,0 +1,7 @@
+﻿//int dayWeek = new Random().Next(1,8);
+int dayWeek = Convert.ToInt32(Console.ReadLine());
+
+if (dayWeek >= 6 && dayWeek <= 7)
+    System.Console.WriteLine("True");
+else
+    System.Console.WriteLine("False");
