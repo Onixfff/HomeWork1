80faa3366adfa7cb276c61f73e593d246f972f0c
Onixfff <danilnikonov01@mail.ru>
2023-06-29 14:38:06 +0200

    Home work 23

---
+++ task23/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+
+Console.WriteLine("Enter num");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter degree");
+int degree = Convert.ToInt32(Console.ReadLine());
+
+Power(num, degree);
+
+void Power(int n, int degree)
+{
+    if (n < 0)
+        n *= -1;
+
+    int count = 1;
+    while (count <= n)
+    {
+        System.Console.Write($"{Math.Pow(count, degree)}");
+
+        if (count != n)
+            System.Console.Write(", ");
+        else
+            System.Console.Write(".");
+
+        count++;
+    }
+}
