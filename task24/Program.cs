f6abd5e16fc4712c61a207017160210fc5a3bac3
fff_ff <fff_ff@gmail.com>
2023-07-04 13:56:07 +0300

    семминар

---
+++ task24/Program.cs
@@ -0,0 +1,15 @@
+﻿int GetSumOfSeries(int from, int to)
+{
+    int result = 0;
+    for(int i = from; i <= to; i++)
+    {
+        result += i;
+    }
+    return result;
+}
+
+System.Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int sum = GetSumOfSeries(1, number);
+System.Console.WriteLine($"Сумма чисел от 1 до {number} = {sum}");
