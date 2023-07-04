7ad06636705e49030238124df7da156023f3973e
fff_ff <fff_ff@gmail.com>
2023-07-04 14:58:30 +0300

    homeWork

---
+++ task27/Program.cs
@@ -0,0 +1,30 @@
+﻿System.Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine(SumDigits(num));
+
+int SumDigits(int num)
+{
+    int num2 = num;
+    int sum = 0;
+    int count = 0;
+
+    if (num2 == 0)
+    {
+        count++;
+    }
+
+    while (num2 > 0)
+    {
+        count++;
+        num2 /= 10;
+    }
+
+    for (int i = 0; i < count; i++)
+    {
+        sum += num % 10;
+        num /= 10;
+    }
+
+    return sum;
+}
