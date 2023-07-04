fd53b702a3e7d1e0463620431ab58db82c8a981f
fff_ff <fff_ff@gmail.com>
2023-07-04 14:10:16 +0300

    homeWork

---
+++ task25/Program.cs
@@ -0,0 +1,11 @@
+﻿int Retue(int num, int pow)
+{
+    int sum = num;
+    for (int i = 1; i < pow; i++)
+    {
+        sum *= num;
+    }
+    return sum;
+}
+
+System.Console.WriteLine(Retue(3,5));
