5ad5f98525ae0349818ca8e88c7b607ef5d320ac
Onixfff <danilnikonov01@mail.ru>
2023-07-11 22:51:34 +0200

    home work

---
+++ task41/Program.cs
@@ -0,0 +1,24 @@
+﻿int count = 0;
+string exit = "break";
+bool checkParse = false;
+
+while(true)
+{
+    Console.WriteLine("Введите число\nДля подсчета введите break");
+    string userInput = Console.ReadLine();
+    if(userInput.ToString() == "break")
+    {
+        Console.Clear();
+
+        if(checkParse == true)
+            System.Console.WriteLine(count);
+        else
+            System.Console.WriteLine("Error. UserInput");
+    }
+    else
+    {
+        int result;
+        checkParse = Int32.TryParse(userInput, out result);
+        count++;
+    }
+}
