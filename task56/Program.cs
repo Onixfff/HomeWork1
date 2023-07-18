c7dd98c11c3bc018f2593c045f4d4cf0971c96d5
Onixfff <danilnikonov01@mail.ru>
2023-07-18 20:23:26 +0200

    Home work

--- task56/Program.cs
+++ task56/Program.cs
@@ -1,2 +1,73 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[,] array = CreateArray(4, 4);
+ShowArray(array);
+int minRows = MinSumRows(array);
+System.Console.WriteLine($"Минимальная сумма находиться в строке - {minRows}");
+
+int[,] CreateArray(int m, int n)
+{
+    Random random = new Random();
+    int minRandom = 1;
+    int maxRandom = 10;
+
+    int[,] array = new int[m, n];
+
+    for(int i = 0; i < m; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            array[i, j] = random.Next(minRandom, maxRandom);
+        }
+    }
+
+    return array;
+}
+
+void ShowArray(int[,] array)
+{
+        int rows = array.GetLength(0);
+        int columns = array.GetLength(1);
+
+        for(int i = 0; i < rows; i ++)
+        {
+            for(int j = 0; j < columns; j++)
+            {
+                if(j < columns -1)
+                {
+                    System.Console.Write(array[i,j] + ", ");
+                }
+                else
+                {
+                    System.Console.Write(array[i,j]);
+                }
+            }
+            System.Console.WriteLine();
+        }
+}
+
+int MinSumRows(int[,] array)
+{
+    int rows = array.GetLength(0);
+    int columns = array.GetLength(1);
+    
+    int index = 0;
+    int sum = 0;
+    int minSum = int.MaxValue;
+
+    for(int i = 0; i < rows; i++)
+    {
+        for(int j = 0; j < columns; j++)
+        {
+            sum += array[i,j];
+        }
+        
+        if(sum < minSum)
+        {
+            minSum = sum;
+            index = i;
+        }
+
+        sum = 0;
+    }
+
+    return index + 1;   
+}
