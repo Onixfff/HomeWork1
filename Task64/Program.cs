e39464634fe6122d42d0e6d1d6bd306b0edfe891
Onixfff <danilnikonov01@mail.ru>
2023-07-25 12:52:28 +0200

    Home work

---
+++ Task64/Program.cs
@@ -0,0 +1,14 @@
+﻿void NatureleNum(int n, int lastN = 1)
+{
+    if(n == lastN - 1)
+    {
+        return;
+    }
+    else
+    {
+        System.Console.Write(n + " ");
+        NatureleNum(--n);
+    }
+}
+
+NatureleNum(8);
---
+++ task66/Program.cs
@@ -0,0 +1,13 @@
+﻿void NatureleNum(int n, int m, int sum = 0)
+{
+    if( n <= m)
+    {
+        NatureleNum(n + 1, m, sum += n);
+    }
+    else
+    {
+        System.Console.WriteLine(sum);
+    }
+}
+
+NatureleNum(4,8);
