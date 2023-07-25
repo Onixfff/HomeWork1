b5e8d52e510fd1aecf3538895e487dedf3d42f81
Onixfff <danilnikonov01@mail.ru>
2023-07-25 13:18:54 +0200

    Home work

---
+++ task68/Program.cs
@@ -0,0 +1,17 @@
+﻿int Acerman(int n, int m)
+{
+    if( n == 0)
+    {
+        return m + 1;
+    }
+    else if( m == 0)
+    {
+        return Acerman(n - 1, 1);
+    }
+    else
+    {
+        return Acerman(n - 1, Acerman(n, m -1));
+    }
+}
+
+System.Console.WriteLine(Acerman(3,2));
---
+++ taskDop/Program.cs
@@ -0,0 +1,10 @@
+﻿void Dop(int firstNum, int secondNum, int N = 4)
+{
+    if(N != 0)
+    {
+        System.Console.Write(firstNum + " ");
+        Dop(secondNum, firstNum + secondNum, --N);
+    }
+}
+
+Dop(6, 10);
